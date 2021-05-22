27d618bdcfc8a634d6e18b6103a9f618b575e9c1
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-22 10:11:55 +0530

    roboshop

--- rs-project.groovy
+++ rs-project.groovy
@@ -1,16 +1,16 @@
 ///
 folder('rs-project') {
-  displayName('rs-project')
+  displayName('Roboshop Project')
   description('rs-project')
 }
 
-pipelineJob('rs-project/VPC') {
+pipelineJob('rs-project/frontend') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-vpc')
+            'url'('https://github.com/imjitthu/tfas-frontend.git')
           }
         }
         'branches' {
@@ -19,179 +19,8 @@ pipelineJob('rs-project/VPC') {
           }
         }
       }
-      'scriptPath'('Jenkinsfile')
+      'scriptPath'('jenkinsfile')
       'lightweight'(true)
     }
   }
 }
-
-pipelineJob('rs-project/FRONTEND') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-frontend')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-pipelineJob('rs-project/ALB') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-alb')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-pipelineJob('rs-project/DATABASES') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-databases')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-
-
-pipelineJob('rs-project/CART') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-cart')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-pipelineJob('rs-project/CATALOGUE') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-catalogue')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-pipelineJob('rs-project/USER') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-user')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-pipelineJob('rs-project/PAYMENT') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-payment')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
-pipelineJob('rs-project/SHIPPING') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps53/_git/terraform-shipping')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
-
